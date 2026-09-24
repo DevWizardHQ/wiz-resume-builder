56e3d9c5f9f4f14448fb555e55d868516626b69f
Shazeedul <syedshazeedul@gmail.com>
2026-09-24 12:53:55 +0600

    refactor next config

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,19 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  serverExternalPackages: ["@react-pdf/renderer"],
+  serverExternalPackages: ["@react-pdf/renderer", "pdfkit"],
   outputFileTracingIncludes: {
-    "/api/export/pdf/[id]": [
-      "./node_modules/pdfkit/js/standard-fonts/**/*",
+    "/api/**/*": [
+      "./node_modules/pdfkit/**/*",
+      "./node_modules/@react-pdf/**/*",
+      "./node_modules/.pnpm/pdfkit@*/**/*",
+      "./node_modules/.pnpm/@react-pdf*/**/*",
+    ],
+    "/**": [
+      "./node_modules/pdfkit/**/*",
+      "./node_modules/@react-pdf/**/*",
+      "./node_modules/.pnpm/pdfkit@*/**/*",
+      "./node_modules/.pnpm/@react-pdf*/**/*",
     ],
   },
 };
